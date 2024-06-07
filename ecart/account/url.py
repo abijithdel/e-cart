7e403b9da081a69e3c93b82abc05258fa6d8a78d
abijith <admin@sphost.cloud>
2024-06-07 17:33:58 -0400

    add signup and edit account

--- ecart/account/url.py
+++ ecart/account/url.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('login/',views.login, name='login')
+    path('login/',views.login, name='login'),
+    path('signup/',views.signup, name='signup'),
+    path('account/',views.account, name='account'),
+    path('edit_account/',views.edit_profile, name="edit_account")
+
 ]
--- ecart/account/views.py
+++ ecart/account/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import logout
+from django.contrib.auth import login as auth_login
 from django.contrib.auth import authenticate,login  as auth_login 
-
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
 # Create your views here.
 def login(req):
     if req.method == 'POST':
@@ -16,3 +21,46 @@ def login(req):
             return render(req,'account/index.html',{'us_pas_not_mach':us_pas_not_mach})
 
     return render(req,'account/login.html')
+
+def signup(req):
+    if req.method == 'POST':
+        fname = req.POST.get('firstname')
+        lname = req.POST.get('lastname')
+        uname = req.POST.get('username')
+        email = req.POST.get('email')
+        pass1 = req.POST.get('password')
+        pass2 = req.POST.get('cpassword')
+        
+        if pass1 != pass2:
+            pass_not_mach='Check Confirm Password'
+            return render(req,'account/signup.html',{'pass_not_mach':pass_not_mach})
+        
+
+        elif User.objects.filter(username=uname).exists():
+            username_taken = 'Username already taken'
+            return render(req, 'account/signup.html', {'username_taken': username_taken})
+        else:
+            users = User.objects.create_user(username=uname, email=email, password=pass1, first_name=fname, last_name=lname)
+            users.save()
+            wel_mess='Welcome',fname,'Login Now'
+            return render(req,'account/login.html',{'wel_mess':wel_mess})
+            
+    return render(req,'account/signup.html')
+
+
+
+def account(req):
+    return render(req,'account/account.html')
+
+
+def edit_profile(req):
+    user = req.user
+    if req.method == 'POST':
+        user.first_name = req.POST.get('first_name')
+        user.last_name = req.POST.get('last_name')
+        user.username = req.POST.get('username')
+        user.email = req.POST.get('email')
+        user.save()
+        saccessf = 'Profile saccessfully Edited'
+        return render(req, 'account/account.html', {'user': user , "saccess":saccessf})
+    return render(req, 'account/account-ed.html', {'user': user})

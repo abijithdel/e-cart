a7e82914d75462c874d74f8ab8dc79dbf3826a98
abijith <admin@sphost.cloud>
2024-06-07 14:31:01 -0400

    add login page

--- ecart/account/views.py
+++ ecart/account/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate,login  as auth_login 
 
 # Create your views here.
+def login(req):
+    if req.method == 'POST':
+        uname = req.POST.get('username')
+        pas = req.POST.get('password')
+        user = authenticate(req,username=uname,password=pas)
+        if user is not None:
+            auth_login(req,user)
+            
+            return render(req,'index.html')
+        else:
+            us_pas_not_mach='Check Username or password'
+            return render(req,'account/index.html',{'us_pas_not_mach':us_pas_not_mach})
+
+    return render(req,'account/login.html')

243a8d67304a66db227cce0ea150529be7c30ed5
abijith <admin@sphost.cloud>
2024-06-08 15:42:18 -0400

    add pro page

--- ecart/store/models.py
+++ ecart/store/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
+    class meta:
+        varbose_name_plural = 'categories'
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='upload/product')
+
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=120, default='', blank=True)
+    phone = models.CharField(max_length=30, default='', blank=True)
+    date = models.DateField(auto_now_add=True)  # Only one option here
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id} for {self.customer.name}"
+
--- ecart/store/views.py
+++ ecart/store/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from . models import Product
 
 # Create your views here.
 
 def home(req):
     return render(req,'index.html')
+
+
+def store_page(req):
+    Products = Product.objects.all()
+    return render(req,'store/products.html',{ 'Products':Products })
